b70713552906f33abaa62146bf1b7f84be15a6b0
Ramon Rahman <ramon@ngsiusa.com>
2015-01-10 21:54:01 -0500

    updated a few things

--- src/ShoeStore/ProductBundle/Controller/ShoeInventoryController.php
+++ src/ShoeStore/ProductBundle/Controller/ShoeInventoryController.php
@@ -11,30 +11,8 @@ class ShoeInventoryController extends Controller
 {
     public function displayAction(Request $request)
     {
-		// create a shoes and give it some dummy data for this example
-        //$shoes = new Shoes();
-        //$shoes->setModelName('Nike Air');
-        //$shoes->setModelNumber('5839584593');
-		//$shoes->setModelImage('/image/something');
-		//$shoes->setMsrPrice(125.25);
-
-		$shoeInventory = $this->getDoctrine()
-        ->getRepository('ShoeStoreProductBundle:Shoes')
-        ->findAllShoes();
-		
-		//fresh task object without dummy data
+    	$em = $this->getDoctrine()->getManager();
 		$shoes = new Shoes();
-		
-        /*$form = $this->createFormBuilder($shoes)
-            ->add('modelName', 'text')
-            ->add('modelNumber', 'text')
-			->add('modelImage', 'text')
-			->add('msrPrice', 'money', array('divisor' => 100, ))
-            ->add('save', 'submit', array('label' => 'Create Shoes'))
-			->add('saveAndAdd', 'submit', array('label' => 'Save and Add'))
-            ->getForm();*/
-		
-		//$form = $this->createForm(new ShoesType());
 		$form = $this->createForm('shoes', $shoes);
 	
 		$form->handleRequest($request);
@@ -43,14 +21,15 @@ class ShoeInventoryController extends Controller
 			// perform some action, such as saving the task to the database
 			
 			$shoe = $form->getData();
-			
-			$em = $this->getDoctrine()->getManager();
 			$em->persist($shoe);
 			$em->flush();
 		
 			return $this->redirect($this->generateUrl('display'));
 		}
 
+		$shoeInventory = $this->getDoctrine()
+		->getRepository('ShoeStoreProductBundle:Shoes')
+		->findAllShoes();
 		
         return $this->render('ShoeStoreProductBundle:ShoeInventory:display.html.twig', array(
                 'form' => $form->createView(), 'allShoes' => $shoeInventory
